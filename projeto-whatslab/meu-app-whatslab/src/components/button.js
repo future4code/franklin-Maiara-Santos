974255fd6b2917da159da94f7dc9e4d9d615d068
Maiara Cruz <maiaracruz96@hotmail.com>
2022-05-27 13:25:14 -0300

    projeto whatslab

---
+++ projeto-whatslab/meu-app-whatslab/src/components/button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleButton = styled.button`
+    color: #dcdcdc;
+    width: 50px;
+    height: 20px;
+    border-radius: 4px;
+    border: none;
+`
+
+
+function Button (){
+    return(
+        <button>Enviar</button>
+    );
+
+}
---
+++ projeto-whatslab/meu-app-whatslab/src/components/section.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleSection = styled.button`
+    color: #fff;
+    width: 100%;
+    height: 700px;
+    
+`
+
+
+function Button (){
+    return(
+        <section>
+            <div>
+                
+            </div>
+        </section>
+    );
+
+}
