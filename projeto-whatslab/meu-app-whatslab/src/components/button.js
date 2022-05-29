ad77bdc507f11f21c6d89b9843dcffc590edcf59
Maiara Cruz <maiaracruz96@hotmail.com>
2022-05-29 19:37:18 -0300

    Interface Pronta

--- projeto-whatslab/meu-app-whatslab/src/components/button.js
+++ projeto-whatslab/meu-app-whatslab/src/components/button.js
@@ -2,17 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyleButton = styled.button`
-    color: #dcdcdc;
-    width: 50px;
-    height: 20px;
+    color:#000;   
+    width: 70px;
+    height: 25px;
     border-radius: 4px;
     border: none;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    background-color: #90ee90;
+    font-family: sans-serif;    
+    font-size: 18px;
+    align-items: flex-end;
+    margin-bottom: 30px;
+    font-weight:600;
+    margin-left: 10px;
+    
+    
+
+    
+   
 `
 
 
 function Button (){
     return(
-        <button>Enviar</button>
+        <StyleButton>Enviar</StyleButton>
     );
 
 }
+
+export default Button;
--- projeto-whatslab/meu-app-whatslab/src/components/section.js
+++ projeto-whatslab/meu-app-whatslab/src/components/section.js
@@ -1,21 +1,107 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from './button';
 
-const StyleSection = styled.button`
-    color: #fff;
+const StyleSection = styled.section`
+    background-color: #dcdcdc;
     width: 100%;
+    height: 950px;
+    
+    
+`
+
+const StyleDiv = styled.div`
+    background-color: #f5f5dc;          
+    text-align: center;    
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    width: 800px;
     height: 700px;
+    margin-left: 560px;
+    
+
+`
+
+const StyleTitle = styled.h1`
+    color: #000
+    font-family: sans-serif;
+    font-size: 35px;     
+    text-align: center;    
+    padding-top: 50px;
+   
+
+`
+
+const StyleInputNome = styled.input`
+    background-color: #fff;
+    border-radius:3px;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #000;       
+    text-align: center;    
+    display: flex;
+    justify-content: center;
+    align-itens: center;
+    width: 100px;
+    height: 40px;
+    margin-bottom: 20px;
+    font-size: 18px;
+    margin-right: 10px;
+  
+
+`
+
+const StyleInputMensagem = styled.input`
+    background-color: #fff;
+    border-radius:3px;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #000;       
+    text-align: center;    
+    display: flex;
+    justify-content: center;
+    align-itens: center;
+    width: 300px;
+    height: 40px;
+    margin-bottom: 20px;
+    font-size: 18px;
+  
+
+`
+
+const StyleLabel = styled.label`
+    color: #000;
+    font-size: 18px;
+    font-weight: 600;
+    margin-bottom: 30px;
+    margin-right:10px
+    
+    
     
 `
 
 
-function Button (){
+
+function SectionChat (){
     return(
-        <section>
-            <div>
-                
-            </div>
-        </section>
+        <StyleSection>
+            <StyleTitle>Protótipo do WhatsLab</StyleTitle> 
+            <StyleDiv>
+                <StyleLabel>Nome</StyleLabel>
+                <StyleInputNome type='text' />
+                <StyleLabel>Mensagem</StyleLabel>   
+                <StyleInputMensagem type='text'/>
+                   
+                  <Button/> 
+            </StyleDiv>
+            
+                    
+        </StyleSection>
+        
     );
 
 }
+
+
+export default SectionChat;
