c0c99bac7655669d5c6634ae688ce52adc8c23cf
Maiara Cruz <maiaracruz96@hotmail.com>
2022-06-07 01:55:48 -0300

    Adicionando Labeforms

---
+++ modulo1/rend-condicional-form/exercicio-labenuforms-1/src/components/Final.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Final () {
+    return(
+        <div>
+            <h1>O FORMULÁRIO ACABOU</h1>
+            <h2>Muito obrigada por participar! Entraremos em contato</h2>
+        </div>
+        
+    )
+}
+
+export default Final;
