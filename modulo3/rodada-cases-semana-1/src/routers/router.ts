89414ddc6caa41e985f9cc4b451fb5c90377f29d
Maiara Cruz <maiaracruz96@hotmail.com>
2022-09-28 10:58:07 -0300

    Subindo Endpoint de Pegar produto pelo nome

--- modulo3/rodada-cases-semana-1/src/routers/router.ts
+++ modulo3/rodada-cases-semana-1/src/routers/router.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import CreateNewProduct from '../endpoints/NewProducts';
+import GetProductbyName from '../endpoints/GetProductByName';
+import GetProducts from '../endpoints/GetProducts';
 
 export const router = Router();
 
 router.post('/create-product', CreateNewProduct);
-
+router.get('/product/:name', GetProductbyName);
+router.get('/product', GetProducts);
