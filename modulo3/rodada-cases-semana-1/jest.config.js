cd1ed853624631a1c12de57a87987e3978617140
Maiara Cruz <maiaracruz96@hotmail.com>
2022-09-29 11:31:52 -0300

    Subindo Ajustes

--- modulo3/rodada-cases-semana-1/jest.config.js
+++ modulo3/rodada-cases-semana-1/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ["<rootDir>/tests/"],
+  roots: ["<rootDir>/tests"],
   transform: {
     "\\.js$": "<rootDir>/node_modules/babel-jest"
   },
