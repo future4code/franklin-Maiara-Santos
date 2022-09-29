55a71fe7ae83752def0373fe42166d78d6074a09
Maiara Cruz <maiaracruz96@hotmail.com>
2022-09-29 10:52:46 -0300

    Adicionando config de testes

--- modulo3/rodada-cases-semana-1/jest.config.js
+++ modulo3/rodada-cases-semana-1/jest.config.js
@@ -1,5 +1,12 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ["<rootDir>/tests/"],
+  transform: {
+    "\\.js$": "<rootDir>/node_modules/babel-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleDirectories: ["node_modules", "src"],
 };
